a08e49ec068e2b1bab88053487c270c3ac3ff678
askar <askarnku@gmail.com>
2024-06-29 16:03:31 -0400

    add script to compare 2 strings

--- chatgpt/all_other.sh
+++ chatgpt/all_other.sh
@@ -1,9 +1,49 @@
 #!/bin/bash
 
 #converts to uppercase
-str="$1"
-str_up="${str^^}"
-echo "$str_up"
+make_upper() {
+	str="$1"
+	str_up="${str^^}"
+	echo "$str_up"
+}
 
 
+#converts to lower
+make_lower() {
+	str="$@"
+	str_up="${str,,}"
+	echo "$str_up"
+}
+
+
+#Check if 2 strings are identical, if not count the number of letters diffrent
+compare_str() {
+        str1="$1"
+        str2="$2"
+
+        if [[ "${#str1}" -ne "${#str1}" ]]; then
+                echo "They are not equal"
+                exit 1
+        fi
+        
+        len="${#str1}"
+        count=0
+        for (( i=0; i<len; i++ )); do
+		local char1="${str1:i:1}"
+		local char2="${str2:i:1}"
+		echo "$char1"
+		echo "$char2"
+                if [ "$char1" != "$char2" ]; then
+                        ((count++))
+                fi
+        done
+        
+        if [ "$count" -ne 0 ]; then
+                echo "They are not equal and they have ${count} differences"
+        else
+                echo "They are equal"
+        fi
+}
+
+compare_str
 
