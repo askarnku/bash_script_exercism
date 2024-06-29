90c5c90767f8eeaf7e5528c94f9d45bf3f44903c
Askar Mamytov <askar.mamytov201@gmail.com>
2024-06-29 14:24:21 -0500

    add chatgpt folder

---
+++ chatgpt/all_other.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#converts to uppercase
+str="$1"
+str_up="${str^^}"
+echo "$str_up"
+
+
+
--- secret_handshake.sh
+++ secret_handshake.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+#if number is less than 1 or greater than 31, it exits
 if [[ $1 -gt 31 ]] || [[ $1 -lt 1 ]]; then
     echo "Number should be between 1 and 31 inclusive"
     exit 1
 fi
 
-declare res=()
+#default messages
+msg=(" wink "" double wink "" close your eyes "" jump ")
+
+#decimal to binary
 binary=""
 
 to_binary() {
@@ -17,11 +21,23 @@ to_binary() {
     echo $binary
 }
 
-# len=
-
-# for (i=)
 
 
+#    I have binary number which is correct form: 1 2 3 4 5
+#    1 num -- check if we should reverse res
+#    2 num -- "jump"
+#    3 num -- "close your eyes"
+#    4 num -- "double blink"
+#    5 num -- "wink"
+
+to_binary $1
+
+for (( i=${#binary}; i>=0; i-- )); do
+    is_pres=${binary:i:-2}
+    if [[ is_pres  ]]
+done
 
 
-to_binary "$@"
+
+
+
